5ffbb9e4c0962f5f4311dd968bd7cee2ab232a64
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-12-11 21:36:55 +0100

    Add click event

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function newSketchPad() {
 
 function createPad(x) {
   if (pixelDiv === undefined) {
-    for (i=0; i < x*x; i++) {
+    for (let i=0; i < x*x; i++) {
       SKETCH_PAD.style.display = 'grid';
       SKETCH_PAD.style.gridTemplateColumns = `repeat(${x}, fit-content(100%))`; // create square sketch pad from user input
       pixelDiv = document.createElement('div');
@@ -30,7 +30,7 @@ function createPad(x) {
     }
   } else {
     SKETCH_PAD.textContent = '';
-    for (i=0; i < x*x; i++) {
+    for (let i=0; i < x*x; i++) {
       SKETCH_PAD.style.gridTemplateColumns = `repeat(${x}, fit-content(100%))`;
       pixelDiv = document.createElement('div');
       pixelDiv.classList.add('pixel');
@@ -38,7 +38,7 @@ function createPad(x) {
     }
   }
   pixelGrid = document.querySelectorAll('.pixel'); // create an array of all the 'pixels'
-  for (i=0; i<pixelGrid.length; i++) {
+  for (let i=0; i<pixelGrid.length; i++) {
     pixelGrid[i].addEventListener('click', function() { // should attribute 'clicked' class to a pixel when clicked on, but is not working
     pixelGrid[i].className = 'clicked';
     }
