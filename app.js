594aac3318736b8eb89f354e8a511f09b15970c2
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-12-11 21:56:46 +0100

    Fix canvas resolution, add mousemove event

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let pixelGrid;
 
 PROMPT_BUTTON.addEventListener('click', newSketchPad);
 
+createPad(32);
+
 function newSketchPad() {
   userSelection = prompt('Combien de pixel de côté souhaitez-vous ? (maximum 100)', '32');
   if (userSelection <= 0) {
@@ -20,26 +22,17 @@ function newSketchPad() {
 }
 
 function createPad(x) {
-  if (pixelDiv === undefined) {
-    for (let i=0; i < x*x; i++) {
-      SKETCH_PAD.style.display = 'grid';
-      SKETCH_PAD.style.gridTemplateColumns = `repeat(${x}, fit-content(100%))`; // create square sketch pad from user input
-      pixelDiv = document.createElement('div');
-      pixelDiv.classList.add('pixel');
-      SKETCH_PAD.appendChild(pixelDiv);
-    }
-  } else {
     SKETCH_PAD.textContent = '';
     for (let i=0; i < x*x; i++) {
-      SKETCH_PAD.style.gridTemplateColumns = `repeat(${x}, fit-content(100%))`;
+      SKETCH_PAD.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+      SKETCH_PAD.style.gridTemplateRows = `repeat(${x}, 1fr)`;
       pixelDiv = document.createElement('div');
       pixelDiv.classList.add('pixel');
       SKETCH_PAD.appendChild(pixelDiv);
     }
-  }
   pixelGrid = document.querySelectorAll('.pixel'); // create an array of all the 'pixels'
   for (let i=0; i<pixelGrid.length; i++) {
-    pixelGrid[i].addEventListener('click', function() { // should attribute 'clicked' class to a pixel when clicked on, but is not working
+    pixelGrid[i].addEventListener('mousemove', function() { // should attribute 'clicked' class to a pixel when clicked on, but is not working
     pixelGrid[i].className = 'clicked';
     }
     );
